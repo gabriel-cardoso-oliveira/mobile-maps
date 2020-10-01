16b57c5711e3888cdac45c670630f97da9089c50
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2020-10-01 11:32:08 -0300

    Config notification push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { Component } from 'react';
 // import { AppLoading } from 'expo';
 // import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 // import {
@@ -11,11 +11,33 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 import './config/ReactotronConfig';
 import { store, persistor } from './store';
 import Routes from './routes';
 
-const Index = () => {
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('25ff1129-29e2-414a-8522-7ed86cd5f9a9');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = (data) => {};
+
+  onOpened = (notification) => {};
+
+  onIds = (id) => {};
   // const [fontsLoaded] = useFonts({
   //   Roboto_400Regular,
   //   Roboto_500Medium,
@@ -27,16 +49,18 @@ const Index = () => {
   //   return <AppLoading />;
   // }
 
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#3e3e3e" />
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#3e3e3e" />
 
-        <Routes />
-      </PersistGate>
-    </Provider>
-  );
-};
+          <Routes />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
 
 export default CodePush({
   checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
